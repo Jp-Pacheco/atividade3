880ebbc35697bc4aae56bdea9c93686caff5634f
Jp-Pacheco <joao2524244@rede.ulbra.br>
2022-11-03 21:34:38 -0300

    calculos doidoes

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title> exercicio1 </title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <form action="exercicio3.php" method="POST">
+            <p id="a">
+                Informe o valor de A:  <input type="number" name="a" />
+            </p>
+            <p id="b">
+                Informe o valor de B:  <input type="number" name="b"  />
+            </p>
+            <p id="c">
+                Informe o valor de C:  <input type="number" name="c"  />
+            </p>
+    
+<input type="submit" value="Calcular" name='calc' id="calc">
+        </form>
+    </body>
+</html>
